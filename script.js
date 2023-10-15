c0be9ac2daa10f3c28f7215d1fc1c7cb7babf4cc
Muhammad Hassan <hassanshakil12@gmail.com>
2023-10-16 01:57:06 +0500

    Dropdown for navbar

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
-let menuBtn = document.querySelector('.menu');
-let crossBtn = document.querySelector('.cross');
-let menu = document.querySelector('.menuSlider');
-let main = document.querySelector('.main');
+const responsiveNavbar = () =>{
+    let menuBtn = document.querySelector('.menu');
+    let crossBtn = document.querySelector('.cross');
+    let menu = document.querySelector('.menuSlider');
+    let main = document.querySelector('.main');
 
-menuBtn.addEventListener('click', function(){
-    menu.style.transform = 'translateX(0%)';
-    menu.style.opacity = 1
-    main.style.overflowY = 'hidden';
-})
+    menuBtn.addEventListener('click', function () {
+        menu.style.transform = 'translateX(0%)';
+        menu.style.opacity = 1
+        main.style.overflowY = 'hidden';
+    })
 
-crossBtn.addEventListener('click', function(){
-    menu.style.transform = 'translateX(-100%)';
-    menu.style.opacity = 0
-    main.style.overflowY = 'auto';
-})
+    crossBtn.addEventListener('click', function () {
+        menu.style.transform = 'translateX(-100%)';
+        menu.style.opacity = 0
+        main.style.overflowY = 'auto';
+    })
+}
+
+const navbarDropdown = () =>{
+    let dropMenu = document.querySelector('.left .dropMenu');
+    let dropDown = document.querySelector('.left .dropdown');
+
+    dropMenu.addEventListener('mouseover', function(){
+        dropDown.style.display = "block";
+    })
+
+    dropMenu.addEventListener('mouseleave', function () {
+        dropDown.style.display = "none";
+    })
+}
+
+// navbarDropdown();
+responsiveNavbar();
